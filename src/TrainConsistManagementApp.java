96d5a20b5056255a5aa00c8f8f720bcbad7e6c36
SauravVenu <souravv2006@gmail.com>
2026-04-07 16:31:50 +0530

    UC2:Hardcoded palindrome

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,24 +2,13 @@ import java.util.Arrays;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-        System.out.println("--- UC16: Bubble Sort (Manual Sorting) ---\n");
-        int[] capacities = {72, 56, 24, 70, 60};
-        int n = capacities.length;
+        System.out.println("--- UC17: Optimized Built-in Sorting ---\n");
+        String[] bogieTypes = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
-        System.out.println("Before Sorting: " + Arrays.toString(capacities));
+        System.out.println("Original: " + Arrays.toString(bogieTypes));
 
-        // Bubble Sort Algorithm
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    // Swap elements
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(bogieTypes);
 
-        System.out.println("After Sorting:  " + Arrays.toString(capacities));
+        System.out.println("Alphabetical Order: " + Arrays.toString(bogieTypes));
     }
 }
