d2876e5b04150550353cc32d3f5e98efeebd38e7
SauravVenu <souravv2006@gmail.com>
2026-04-07 16:35:16 +0530

    UC2:Hardcoded palindrome

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,17 +1,24 @@
 public class TrainConsistManagementApp {
-    public static boolean linearSearch(String[] arr, String key) {
-        for (String id : arr) {
-            if (id.equals(key)) return true;
+    public static int binarySearch(String[] arr, String key) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = key.compareTo(arr[mid]);
+
+            if (res == 0) return mid;
+            if (res > 0) low = mid + 1;
+            else high = mid - 1;
         }
-        return false;
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println("--- UC18: Linear Search (O(n)) ---\n");
-        String[] bogies = {"BG101", "BG205", "BG309", "BG412", "BG550"};
-        String target = "BG309";
+        System.out.println("--- UC19: Binary Search (O(log n)) ---\n");
+        // Pre-sorted array is a requirement
+        String[] sortedBogies = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+        String target = "BG412";
 
-        boolean found = linearSearch(bogies, target);
-        System.out.println("Searching for " + target + "... Found: " + found);
+        int result = binarySearch(sortedBogies, target);
+        System.out.println("Bogie " + target + (result != -1 ? " found at index " + result : " not found."));
     }
 }
